f8938358e22b6122b1c884c5b8c6a1c98e71c147
Nova <56466589+nova-fe@users.noreply.github.com>
2025-01-10 07:04:06 +0000

    html을 반복문으로 줄이고 싶으면 map

--- fresh/app/list/page.js
+++ fresh/app/list/page.js
@@ -5,15 +5,27 @@
  * 3. 그 안에 레이아웃 넣기
  */
 export default function List() {
+  let 상품 = ['Tomatoes', 'Pasta', 'Coconut']
+  let arr = [2,3,4]
+  
+  let b = arr.map((a) => {
+    // return 에 담은걸 array로 담아줌
+    return 10;
+  })
+  console.log(b); // [10,10,10]
+
   return (
     <div>
-      <h2 className="title">Products</h2>
-      <div className="food">
-        <h4>상품명 $40</h4>
-      </div>
-      <div className="food">
-        <h4>상품명 $40</h4>
-      </div>
+      <h2 className="title">상품목록</h2>
+      {
+        상품.map((a, i) => {
+          return (
+            <div className="food">
+              <h4>{a} $40</h4>
+            </div>
+          )
+        })
+      }
     </div>
   );
 }
