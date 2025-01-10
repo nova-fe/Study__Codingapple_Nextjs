dc5c5f9523d95363716bcb8a1f8da75b047ea6af
Nova <56466589+nova-fe@users.noreply.github.com>
2025-01-10 08:37:38 +0000

    Component에 데이터 전해주려면 props

---
+++ fresh/app/cart/data.js
@@ -0,0 +1,5 @@
+let age = 20;
+let name = 'kim';
+
+export {name};
+export default age;
--- fresh/app/cart/page.js
+++ fresh/app/cart/page.js
@@ -1,17 +1,35 @@
+// 'use client'
+// 가장 상단에 use client 를 넣고 만든 파일은 클라이언트 컴포넌트가 됨
+
 export default function Cart() {
+  let 장바구니 = ['Tomatoes', 'Pasta']
+
   return (
     <div>
       <h4 className="title">Cart</h4>
-      <div className="cart-item">
-        <p>상품명</p>
-        <p>$40</p>
-        <p>1개</p>
-      </div>
-      <div className="cart-item">
-        <p>상품명</p>
-        <p>$40</p>
-        <p>1개</p>
-      </div>
+      <CartItem item={장바구니[0]} />
+      <CartItem item={장바구니[1]} />
+      <Banner content="삼성카드" />
+      <Banner content="현대카드" />
+      <Button color="blue" />
     </div>
   )
-} 
+}
+
+function Banner(props) {
+  return <h5>{props.content} 결제 행사중</h5>
+}
+
+function CartItem(props) {
+  return (
+    <div className="cart-item">
+      <p>{props.item}</p>
+      <p>$40</p>
+      <p>1개</p>
+    </div>
+  )
+}
+
+function Button(props) {
+  return <button style={{backgroundColor : props.color}}>버튼</button>
+}
