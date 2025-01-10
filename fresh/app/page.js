0e2ade96fc588e5ebf61aeec6418a58a787cadc6
Nova <56466589+nova-fe@users.noreply.github.com>
2025-01-10 06:31:24 +0000

    페이지 레이아웃 만들기 (React 기초문법)

--- fresh/app/page.js
+++ fresh/app/page.js
@@ -1,7 +1,12 @@
 export default function Home() {
+  let name = 'park'
+  let link = 'http://google.com'
+
   return (
     <div>
-
+      <h4 style={{color: "red", fontSize: "30px"}}>애플후레시</h4>
+      <p className="title-sub">by dev {name}</p>
+      <a href={link}>링크</a>
     </div>
   );
 }
