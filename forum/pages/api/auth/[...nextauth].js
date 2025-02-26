9e8ad1d59a9529708ca020cc038308ef0c2cb782
Nova <heetask@gmail.com>
2025-02-26 17:43:20 +0900

    회원기능 만들기 : Auth.js 사용한 소셜로그인

---
+++ forum/pages/api/auth/[...nextauth].js
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+export const authOptions = {
+  providers: [  // 구현하고싶은 로그인 방식
+    GithubProvider({  // Github 로그인
+      clientId: 'Ov23liZpHgPsxYoLci41',
+      clientSecret: 'c0ad81e9cdbc392e1a93a74aef6f4cb11a194fb9',
+    }),
+  ],
+  secret : 'yellow3333blue'
+};
+export default NextAuth(authOptions); 
